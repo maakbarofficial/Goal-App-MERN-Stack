43214c1eeeaaf87adf6b14fac6255d6f240218e3
Muhammad Ali Akbar <maakbarofficial@gmail.com>
2024-04-13 17:18:57 +0500

    Completed Frontend

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -38,15 +38,13 @@ const updateGoal = asyncHandler(async (req, res) => {
     throw new Error("Goal not found");
   }
 
-  const user = await User.findById(req.user.id);
-
-  if (!user) {
+  if (!req.user) {
     res.status(401);
     throw new Error("User not found");
   }
 
   // Checking only logged in user matches the goal of user
-  if (goal.user.toString() !== user.id) {
+  if (goal.user.toString() !== req.user.id) {
     res.status(401);
     throw new Error("User not authorized");
   }
@@ -67,15 +65,13 @@ const deleteGoal = asyncHandler(async (req, res) => {
     throw new Error("Goal not found");
   }
 
-  const user = await User.findById(req.user.id);
-
-  if (!user) {
+  if (!req.user) {
     res.status(401);
     throw new Error("User not found");
   }
 
   // Checking only logged in user matches the goal of user
-  if (goal.user.toString() !== user.id) {
+  if (goal.user.toString() !== req.user.id) {
     res.status(401);
     throw new Error("User not authorized");
   }
